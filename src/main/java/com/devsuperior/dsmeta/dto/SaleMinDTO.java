466975400bf0bae5465ed401cb119386db50eaaf
Edu-Paz <eduardopazcarv@gmail.com>
2026-05-27 06:49:50 -0300

    feat: add sellerName on SaleMinDTO

--- src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
+++ src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
@@ -3,23 +3,27 @@ package com.devsuperior.dsmeta.dto;
 import java.time.LocalDate;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.entities.Seller;
 
 public class SaleMinDTO {
 
 	private Long id;
-	private Double amount;
 	private LocalDate date;
-	
-	public SaleMinDTO(Long id, Double amount, LocalDate date) {
+	private Double amount;
+	private String sellerName;
+
+	public SaleMinDTO(Long id, LocalDate date, Double amount, String sellerName) {
 		this.id = id;
-		this.amount = amount;
 		this.date = date;
+		this.amount = amount;
+		this.sellerName = sellerName;
 	}
 	
 	public SaleMinDTO(Sale entity) {
 		id = entity.getId();
 		amount = entity.getAmount();
 		date = entity.getDate();
+		sellerName = entity.getSeller().getName();
 	}
 
 	public Long getId() {
